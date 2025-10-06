bde7d58064d0f7a7baf846e09f6b9e66ddb004a2
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-10-06 10:50:24 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app
+  .get("/", (req, res) => {
+    return res.status(200).send("Local get");
+  })
+  .post("/register", (req, res) => {
+    res.status(200).send("Local post register");
+  });
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server running on port  ${port}`);
+});
