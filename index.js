ceb47a1e0213351c396fe087635c257b9c3b2ba9
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-10-07 08:48:48 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app
   .get("/", (req, res) => {
     return res.status(200).json("Local get");
   })
   .post("/register", (req, res) => {
-    res.status(200).json("Local post register");
+    res.status(200).json(req.body);
   });
 
-const port = 8080;
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Server running on port  ${port}`);
 });
+
+// TODO: Add .env config
