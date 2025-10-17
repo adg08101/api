55e2e8e8683e6dd1ff4b3d10f5292e077ed9dd49
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-10-17 09:33:28 -0300

    login cookie

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
